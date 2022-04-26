5929f5a31dab741a0d94d97004ab984a50029108
jaladhis <1978633500@qq.com>
2022-04-26 14:53:39 +0800

    modified reload

--- img_reload.py
+++ img_reload.py
@@ -4,12 +4,12 @@ import imghdr
 import os
 import requests
 import argparse
-import re
+import copy
 from img_down import get_format_path, re_find,IMG_PATH,ERROR_IMG_PATH,IMG_EXTS
 from img_confirm import NOT_AN_IMAGE_PATH
 
 def reload_image(error_img_log:dict):
-    for pid_str,values in error_img_log.copy().items():
+    for pid_str,values in copy.deepcopy(error_img_log).items():
         pid = int(pid_str)
         new_values = []
         os.makedirs(os.path.join(IMG_PATH,'%0d'%pid), exist_ok=True)
@@ -28,7 +28,7 @@ def reload_image(error_img_log:dict):
                 print('%d\tAn img dowloaded.'%pid)
             else:
                 flag = False
-                new_values.append({values:status_code})
+                new_values.append({list(value)[0]:status_code})
                 print('%d\tAn img failed.'%pid)
         if flag:
             error_img_log.pop(pid_str)
@@ -37,18 +37,18 @@ def reload_image(error_img_log:dict):
             error_img_log[pid_str] = new_values
             print('%d\tSome img(s) failed to be downloaded.'%pid)
         
-def recover_url(pid:int,fn:str):
-    imgs = re_find(pid)
+def recover_url(fn:str,imgs):
     for img in imgs:
         if fn in img:
             return img
     return ''
             
 def reload_url(error_imgs):
-    for pid_str,fns in error_imgs.copy().items():
+    for pid_str,fns in copy.deepcopy(error_imgs).items():
         pid = int(pid_str)
+        imgs = re_find(pid)
         for fn in fns:
-            url = recover_url(pid,fn)
+            url = recover_url(fn,imgs)
             img_path = get_format_path(url,pid)
             try:
                 img_get = requests.get(url,verify=False)
@@ -61,12 +61,14 @@ def reload_url(error_imgs):
                 with open (img_path,'rb') as f:
                     is_img = imghdr.what(f)
                 if is_img:    
-                    values = error_imgs[pid_str].remove(fn)
-                    if values:
-                        error_imgs[pid_str] = values
+                    error_imgs[pid_str].remove(fn)
+                    if error_imgs[pid_str]:
+                        pass
                     else:
                         error_imgs.pop(pid_str)
                     print('%d\tAn img reloaded.'%pid)
+            else:
+                print('%d\tAn img failed to be reloaded.'%pid)
 
 def main():
     parser = argparse.ArgumentParser('T-hole.red Img reload')
@@ -80,26 +82,36 @@ def main():
             with open(ERROR_IMG_PATH,'r')as f:
                 error_img_log = json.load(f)
             reload_image(error_img_log)
-        except:
+        except Exception as e:
+            print(e)
+            print(e.__traceback__.tb_frame.f_globals["__file__"])
+            print(e.__traceback__.tb_lineno)
             sys.exit()
         try:
             with open(ERROR_IMG_PATH,'w+')as f:
                 json.dump(error_img_log,f)
-        except:
-            pass
+        except Exception as e:
+            print(e)
+            print(e.__traceback__.tb_frame.f_globals["__file__"])
+            print(e.__traceback__.tb_lineno)
         
     if args.e:    
         try:
             with open(NOT_AN_IMAGE_PATH,'r')as f:
                 error_imgs = json.load(f)
             reload_url(error_imgs)
-        except:
+        except Exception as e:
+            print(e)
+            print(e.__traceback__.tb_frame.f_globals["__file__"])
+            print(e.__traceback__.tb_lineno)
             sys.exit()
         try:
             with open(NOT_AN_IMAGE_PATH,'w+')as f:
                 json.dump(error_imgs,f)
-        except:
-            pass
+        except Exception as e:
+            print(e)
+            print(e.__traceback__.tb_frame.f_globals["__file__"])
+            print(e.__traceback__.tb_lineno)
 
 if __name__ == '__main__':
     main()
